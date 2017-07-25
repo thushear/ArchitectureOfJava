271e2245f3b2e3f5110f6a7735e1a25c87338ec0
kongming <kongming@jd.com>
2017-07-25 11:04:51 +0800

    spring data

--- ml/src/main/java/com/datumbox/opensource/segment/HanLPKeyWord.java
+++ ml/src/main/java/com/datumbox/opensource/segment/HanLPKeyWord.java
@@ -1,10 +1,18 @@
 package com.datumbox.opensource.segment;
 
+import java.util.List;
+
 /**
  * Created by kongming on 2017/7/21.
  */
 public class HanLPKeyWord {
 
+
+
+    public static List<String> extractKeyWord(String content,Integer topN){
+
+        return null;
+    }
 
 
 
